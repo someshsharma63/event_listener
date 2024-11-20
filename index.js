4ee8ef52493e3770bd5755075e50384a2e15ccb4
Somesh <someshdevera98@gmail.com>
2024-11-20 13:45:47 +0530

    add event listener in js

---
+++ index.js
@@ -0,0 +1,17 @@
+let button=document.getElementById('btn');
+
+// button.addEventListener('click',change);
+
+// function change()
+// {
+//     let target=document.getElementById("test");
+//     target.style="background:yellow";
+// }
+
+
+button.addEventListener('click',()=>
+{
+
+    let target=document.getElementById("test");
+    target.style="background:blue"
+})
